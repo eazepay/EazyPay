c4e95a3eb3e09a2419c89bca62e903ca56d5e248
Samuel Ojo <88172184+greatsamist@users.noreply.github.com>
2023-09-13 18:22:01 +0100

    Update EazePayToken.sol

--- packages/hardhat/contracts/EazePayToken.sol
+++ packages/hardhat/contracts/EazePayToken.sol
@@ -1,13 +1,40 @@
 // SPDX-License-Identifier: MIT License
-pragma solidity 0.8.17;
+pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract EazePay is ERC20 {
     address public controller;
     // Owner of the contract
     address public owner;
+
+    uint256 public userId = 1000;
+
+    // ====================== MAPPINGS ===================== //git
     // Mapping of currency symbols to their respective prices
     mapping(string => uint256) public currencyPrices;
+    // Mapping of user IDs to their user addresses
+    mapping(uint256 => address) public userId_Address;
+    // Mapping of user IDs to their usernames;
+    mapping(uint256 => string) public userId_Username;
+    //
+    mapping(uint256 => uint256) public idToBalance;
+
+    // ====================== EVENTS ===================== //
+
+    // Event to log currency withdrwaw
+    event CurrencyWithdraw(
+        address indexed user,
+        string currencySymbol,
+        uint256 amount,
+        uint256 tokens
+    );
+
+    event rechargedToken(
+        address indexed user,
+        string currencySymbol,
+        uint256 amount
+    );
+    event joined(address user, uint256 id);
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         controller = msg.sender;
@@ -28,11 +55,11 @@ contract EazePay is ERC20 {
         _;
     }
 
-    function mint(address account, uint256 amount) public onlyController {
+    function mint(address account, uint256 amount) internal  {
         _mint(account, amount);
     }
 
-    function burn(address account, uint256 amount) public onlyController {
+    function burn(address account, uint256 amount) internal  {
         _burn(account, amount);
     }
 
@@ -50,7 +77,7 @@ contract EazePay is ERC20 {
 
     // Function to exchange currency for tokens
     function withdraw(
-        address user,
+        uint256 id,
         string memory currencySymbol,
         uint256 amount
     ) external {
@@ -58,42 +85,44 @@ contract EazePay is ERC20 {
         require(currencyPrice > 0, "Currency not supported");
 
         // Calculate the required token amount based on the currency and amount
-        uint256 requiredTokens = currencyPrice * amount;
+        uint256 requiredTokens = amount / currencyPrice;
 
         // Check if the user has enough tokens
-        require(balanceOf(user) >= requiredTokens, "Insufficient balance");
-
+        require(idToBalance[id] >= requiredTokens, "Insufficient balance");
+        idToBalance[id] -= requiredTokens;
         // burn user tokens
-        burn(user, requiredTokens);
+        burn(userId_Address[id], requiredTokens);
 
-        // emit event
-        emit CurrencyWithdraw(user, currencySymbol, amount, requiredTokens);
+        // emit withdraw event
+        emit CurrencyWithdraw(
+            userId_Address[id],
+            currencySymbol,
+            amount,
+            requiredTokens
+        );
     }
 
-    // Event to log currency withdrwaw
-    event CurrencyWithdraw(
-        address indexed user,
-        string currencySymbol,
-        uint256 amount,
-        uint256 tokens
-    );
-
     function recharge(
-        address user,
+        uint256 id,
         string memory currencySymbol,
         uint256 amount
     ) external {
         uint256 currencyPrice = currencyPrices[currencySymbol];
         // Calculate the recharge token amount based on the currency and amount
-        uint256 totalTokenPrice = currencyPrice * amount;
-        mint(user, totalTokenPrice);
+        uint256 totalTokenPrice = amount / currencyPrice;
+        idToBalance[id] += totalTokenPrice;
+        mint(msg.sender, totalTokenPrice);
 
-        emit rechargedToken(user, currencySymbol, amount);
+        // emit recharge event
+        emit rechargedToken(msg.sender, currencySymbol, amount);
     }
 
-    event rechargedToken(
-        address indexed user,
-        string currencySymbol,
-        uint256 amount
-    );
+    function join(string memory username) external {
+        userId_Address[userId] = msg.sender;
+        userId_Username[userId] = username;
+        idToBalance[userId];
+        userId++;
+        emit joined(msg.sender, userId);
+    }
 }
+
